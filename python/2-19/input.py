41d0a2ae1bf652ea587ca8f8e87aaa1a850a0a19
Guailoudou <108342285+Guailoudou@users.noreply.github.com>
2025-02-19 10:00:52 +0800

    0

---
+++ python/2-19/input.py
@@ -0,0 +1,17 @@
+# import json
+# 啊啊 = "啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊啊"
+a,b = 3,4
+a,b = b,a
+print("a=",a,"b=",b)
+c = eval(input("输入~"))
+# c = input("输入c"),c=int(c)
+d = input("输入d")
+# sep为不同变量间隔，end为末尾填充
+print("c-d：",c-int(d),sep=' ',end='\n')
+
+print("my name")
+print("zhan")
+
+print("my name",end=":")
+print("zhan3")
+
---
+++ python/2-19/turtle.py
@@ -0,0 +1,5 @@
+import turtle
+turtle.pendown() #落笔
+for i in range(36):
+    turtle.circle(100,360)
+    turtle.right(10)
