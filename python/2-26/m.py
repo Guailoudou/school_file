d316989ce727c9a76be765cac9bf6a53126a1d7e
Guailoudou <108342285+Guailoudou@users.noreply.github.com>
2025-02-26 09:59:42 +0800

    t

--- python/2-26/m.py
+++ python/2-26/m.py
@@ -25,3 +25,15 @@ print(s.find("2"))\
 s="长-安-三-万%里"
 m=s.replace("-","").replace("%","")
 print(m)
+
+import random	 # 导入随机模块
+surname='赵，钱，孙，李，周，吴，郑，王，冯，陈，褚，卫，蒋，沈，韩，杨，朱，秦，尤，许'
+second='中，万，斯，近，元，伟，丽，利，国，士，文，连，百，宏，可，立，成，海'
+third='隆，智，渝，顺，乐，天，杰，夫，煜，兵，思，霆，炜，祺，亮，剀，炫，翔，维，瑞，韬，嘉，林，庆，玮'
+surname= surname.replace("，","")
+second= second.replace("，","")
+third= third.replace("，","")
+name = random.choice(surname)+random.choice(second)+random.choice(third)
+print(name)
+name2 = random.choice(surname)+random.choice(third)
+print(name2)
