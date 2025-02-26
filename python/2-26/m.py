fef9e5b391bbd8159fb73ed908bcb46e4a68c95b
Guailoudou <108342285+Guailoudou@users.noreply.github.com>
2025-02-26 09:50:34 +0800

    temp

--- python/2-26/m.py
+++ python/2-26/m.py
@@ -16,4 +16,12 @@ s="李白说：大鹏一日同风起，扶摇直上九万里。常建说：恐�
 西游长安城。"
 print(s.find("长安"))
 print(s.rfind("长安"))
-print(s.find("2"))
+print(s.find("2"))\
+
+# s="燕子去了，有再来地时候；杨柳枯了，有再青地时候；桃花谢了，有再开地时候。"
+# m= s.replace("地","的")
+# print(m)
+
+s="长-安-三-万%里"
+m=s.replace("-","").replace("%","")
+print(m)
